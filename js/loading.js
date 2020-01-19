73f7b976e3e984699cc887f49d8e2c3b0f6c207d
pocho171 <32076706+pocho171@users.noreply.github.com>
2020-01-19 15:59:06 -0400

    Create loading.js

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+// <![CDATA[
+        function preloader(){
+            document.getElementById("loading").style.display = "none";
+            document.getElementById("zoliframe").style.display = "block";
+        }//preloader
+        window.onload = preloader;
+// ]]> 
